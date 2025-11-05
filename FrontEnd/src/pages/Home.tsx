6a25274625c533a7dd9c35af82daf67e36d27588
Abdul Samad <junaid.abdulsamad@gmail.com>
2025-11-06 04:39:47 +0500

    clerk-done

---
+++ FrontEnd/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import Hero from '../components/sections/Hero'
+
+export default function Home() {
+  return (
+    <main className="bg-[#0c0c0c] min-h-[calc(100vh-4rem)]">
+      <Hero />
+    </main>
+  )
+}
